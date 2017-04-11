eda32081d65d61378dfdd8c74b384be797e48faa
cricketlong <cricketlong@gmail.com>
2017-04-11 16:36:05 +0200

    created entries for campaign, adgroup and keywords

--- cks/urls.py
+++ cks/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^full_table$', views.full_table, name='full_table'),
+    url(r'^([\d]+)[/]?$', views.campaign, name='campaign'),
+    url(r'^([\d]+)/([\d]+)[/]?$', views.adgroup, name='adgroup'),
+    url(r'^([\d]+)/([\d]+)/([\d]+)[/]?$', views.keyword, name='keyword')
 ]
